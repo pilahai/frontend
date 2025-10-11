14e9d000bd7ddea3753a9ed974e632ad454f1cb3
upida <salmamufida34@gmail.com>
2025-10-11 20:17:18 +0700

    test pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,22 +3,36 @@ import mkcert from 'vite-plugin-mkcert'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devServer: {
-    https: true
+    // https: true
   },
   vite: {
     plugins: [
       mkcert()
     ]
   },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'manifest',
+          href: (process.env.NUXT_APP_BASE_URL !== undefined && process.env.NUXT_APP_BASE_URL != '/' ? (process.env.NUXT_APP_BASE_URL + '/') : '') + 'manifest.webmanifest',
+        }
+      ]
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   modules: ['@vite-pwa/nuxt'],
   pwa: {
+    strategies: 'generateSW',
+    registerType: 'autoUpdate',
     manifest: {
-      name: 'Pilahai PWA',
+      id: '/',
+      start_url: '/',
+      name: 'Pilahai',
       short_name: 'Pilahai',
-      description: 'Pilahai PWA',
+      description: 'Pilahai',
       theme_color: '#ffffff',
       icons: [
         {
@@ -31,10 +45,17 @@ export default defineNuxtConfig({
           sizes: '512x512',
           type: 'image/png',
         },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
       ],
     },
     workbox: {
       globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      navigateFallback: null,
     },
     devOptions: {
       enabled: true,
